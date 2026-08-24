2076d5f751893cdaab71fcbbbf7817e0bfa1229c
cigretteaftrsex-web <cigretteaftrsex@gmail.com>
2026-08-25 01:42:23 +0630

    Delete test_full.cjs

--- test_full.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('bot.ts', 'utf8');
-
-const strictRegex = /\/\/\s*ATOM sometimes marks the daily point item with action\/button label\s*if\s*\(item\.action\s*===\s*'Claim'\s*\|\|\s*item\.buttonText\s*===\s*'Claim'\s*\|\|\s*item\.button_text\s*===\s*'Claim'\s*\|\|\s*item\.buttonText\s*===\s*'ရယူမည်'\)\s*return\s*true;\s*return\s*false;\s*\/\/\s*Stricter checking so we don't accidentally get an unclaimable
