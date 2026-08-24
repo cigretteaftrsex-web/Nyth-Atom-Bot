0e78ddd5a4aceba77c3bf92cc239be07e0906f18
cigretteaftrsex-web <cigretteaftrsex@gmail.com>
2026-08-25 01:41:34 +0630

    Delete fix_score.cjs

--- fix_score.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('bot.ts', 'utf8');
-
-const oldMask = `const userIdDisplay = b64UserId.length > 8 ? 
-      b64UserId.substring(0, 4) + "****" + b64UserId.substring(b64UserId.length - 3) : b64UserId;`;
-const newMask = `const userIdDisplay = b64UserId.length
